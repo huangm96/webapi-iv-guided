86996ccd3c6f48223fcba7313faefbcc11be95e5
Min-Huang <minh.huang96@gmail.com>
2019-10-03 10:25:59 -0700

    deploying app

--- data/db.js
+++ data/db.js
@@ -1,5 +1,12 @@
 const knex = require('knex');
 
-const config = require('../knexfile.js').development;
+const secrets = require("../config/secrets.js");
+
+const environment = secrets.environment || 'development'
+const config = require('../knexfile.js')[environment];
+
+// {development}
+// config.development === config["development"]
+
 
 module.exports = knex(config);
